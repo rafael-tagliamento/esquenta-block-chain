67a9d2c064c6d477365aecedd1fc04543c3e4ae1
mvl18 <mvl18@proton.me>
2025-08-31 16:44:00 -0300

    fix: Arruma o caminho para o MarkdownRenderer

--- frontend/src/components/home/HeroSection.jsx
+++ frontend/src/components/home/HeroSection.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import ArrowRightIcon from '../icons/ArrowRightIcon';
-import MarkdownRenderer from './MarkdownRenderer';
+import MarkdownRenderer from '../MarkdownRenderer';
 
 const HeroSection = ({ textTranslateY, textOpacity }) => {
 	const [accountId, setAccountId] = useState('');
