b7d49644fedbf3153336d0a368a4793c893a83a4
mvl18 <mvl18@proton.me>
2025-08-31 21:41:12 -0300

    feat: Arruma nomes e remove inutil

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException
-from typing import Optional
 from rag import RAGSystem
 from stellar_connector import StellarDataRetriever
 from fastapi.middleware.cors import CORSMiddleware
--- frontend/src/components/home/DescriptionSection.jsx
+++ frontend/src/components/home/DescriptionSection.jsx
@@ -1,5 +1,4 @@
 import "../../App.css";
-import React from "react";
 
 const DescriptionSection = () => {
   return (
